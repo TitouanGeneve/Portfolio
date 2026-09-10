6a4bf16b8de00433eeca95a3747ad6f164df758a
gtdev <165834755+Themostrandom@users.noreply.github.com>
2026-09-10 19:42:55 +0200

    Ajout photographies + form fromspree

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-document.getElementById('contactForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    
-    alert('Merci Titouan ! Votre message a bien été envoyé (démo).');
-    
-    this.reset();
-});
-
 console.log("Portfolio de Titouan Genève chargé avec succès.");
